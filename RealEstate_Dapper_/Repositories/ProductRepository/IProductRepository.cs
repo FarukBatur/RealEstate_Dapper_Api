f937e6ee0e7bb4e63e4e3f36c1fd4f02369835af
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2024-02-21 14:12:47 +0300

    Data extraction from related tables and the home page layout section were created.

---
+++ RealEstate_Dapper_/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate_Dapper_.Dtos.ProductDtos;
+
+namespace RealEstate_Dapper_.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoriesAsync();
+    }
+}
---
+++ RealEstate_Dapper_/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using RealEstate_Dapper_.Dtos.ProductDtos;
+using RealEstate_Dapper_.Models.DapperContext;
+
+namespace RealEstate_Dapper_.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            string query = "Select *From Product";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoriesAsync()
+        {
+            string query = "Select ProductId,Title,Price,City,District,CategoryName From Product inner join Category on Product.ProductCategory = Category.CategoryId";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
