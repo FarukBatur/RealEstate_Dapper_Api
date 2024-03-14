f0849069207b85addd1b62e12e307d903dd0f3c4
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2024-03-14 16:01:06 +0300

    create service repository

---
+++ RealEstate_Dapper_/Repositories/ServiceRepository/ServiceRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using RealEstate_Dapper_.Dtos.CategoryDtos;
+using RealEstate_Dapper_.Models.DapperContext;
+using RealEstate_Dapper_Api.Dtos.ServicesDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        private readonly Context _context;
+
+        public ServiceRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void CreateService(CreateServiceDto createServiceDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteService(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ResultServiceDto>> GetAllServiceAsync()
+        {
+            string query = "Select *From Service";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultServiceDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public Task<GetByIdServiceDto> GetServiceAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
