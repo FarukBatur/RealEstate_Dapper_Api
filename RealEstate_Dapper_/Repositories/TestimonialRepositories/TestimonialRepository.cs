249a74041ffa46b87a8b9dcdafb492710653e7f9
Ömer Faruk BATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2024-04-16 16:57:48 +0300

    Customer Comments service and consume process was done.

---
+++ RealEstate_Dapper_/Repositories/TestimonialRepositories/TestimonialRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using RealEstate_Dapper_.Models.DapperContext;
+using RealEstate_Dapper_Api.Dtos.TestimonialDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepositories
+{
+    public class TestimonialRepository : ITestimonialRepository
+    {
+        private readonly Context _context;
+
+        public TestimonialRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultTestimonialDtos>> GetAllTestimonialAsync()
+        {
+            string query = "Select *From Testimonial";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultTestimonialDtos>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
